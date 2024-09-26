678d1486643e9a628d2b28c2694d99fc3b908fc4
Luiz Henrique <131896193+Luhenrao@users.noreply.github.com>
2024-09-26 08:21:36 -0300

    Delete demo directory

--- demo/src/main/java/com/example/demo/exception/EntityNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.exception;
-
-public class EntityNotFoundException extends Exception {
-
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-}
