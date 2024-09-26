c323a01300bf4f29911cf1ea49031f936b23bb08
Luiz Henrique <131896193+Luhenrao@users.noreply.github.com>
2024-09-26 12:58:04 -0300

    Delete dupl directory

--- dupl/demo/src/main/java/com/java02/demo/Exception/PersonagemNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.java02.demo.Exception;
-
-public class PersonagemNotFoundException extends Exception {
-    public PersonagemNotFoundException(String id) {
-        super("Personagem não encontrado " + id);
-    }
-}
--- dupl/demo/src/main/java/com/java02/demo/Exception/PlanetaNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.java02.demo.Exception;
-
-public class PlanetaNotFoundException extends Exception {
-    public PlanetaNotFoundException(String id) {
-        super("Planeta não encontrado " + id);
-    }
-}
