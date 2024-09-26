03dd4d020cc24bcac707787f21064611886e41b8
Luiz Henrique <131896193+Luhenrao@users.noreply.github.com>
2024-09-26 12:58:21 -0300

    Delete atividade-em-dupla-01-updated-alysson-rafael-gabriel-pereira directory

--- atividade-em-dupla-01-updated-alysson-rafael-gabriel-pereira/src/main/java/com/example/demo/exeption/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.exeption;
-
-public class ResourceNotFoundException extends Exception {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
